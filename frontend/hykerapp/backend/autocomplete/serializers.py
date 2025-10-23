765f68c926caec099e43149b89e776e003ce5545
apras4 <apras4@uic.edu>
2025-10-23 00:05:57 -0500

    test

--- frontend/hykerapp/backend/autocomplete/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SearchTerm
-
-class SearchTermSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SearchTerm
-        fields = ['term', 'frequency']
