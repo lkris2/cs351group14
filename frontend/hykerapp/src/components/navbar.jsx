62dbef5c5dec284d83fbdfde8af18dcd5282ccf0
apras4 <apras4@uic.edu>
2025-10-22 22:45:19 -0500

    combined main + find ride

--- frontend/hykerapp/src/components/navbar.jsx
+++ frontend/hykerapp/src/components/navbar.jsx
@@ -1,12 +1,16 @@
+import hykerLogo from "../assets/hykerLogo.png";
+import { Link } from "react-router-dom";
+
 export default function Navbar(){
     return(
         <nav className = "bg-[#58062F] text-white flex justify-between items-center px-10 py-4">
-            <div className="flex items-center gap-3">
-                <h1 className="text-xl font-bold">Hyker</h1>
+             <div className="flex items-center gap-3">
+                <img className="w-24 h-24" src={hykerLogo}></img>
             </div>
+
             <div className="flex gap-10">
                 <a className="hover:underline underline-offset-4 text-lg">See Requests</a>
-                <a className="hover:underline underline-offset-4 text-lg">Ride</a>
+                <a className="hover:underline underline-offset-4 text-lg" ><Link to="/RidePage">Ride</Link></a>
                 <a className="hover:underline underline-offset-4 text-lg">About</a>
                 <a className="hover:underline underline-offset-4 text-lg">Login</a>
             </div>
